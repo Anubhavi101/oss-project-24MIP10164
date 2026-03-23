f577aad0289353cee765952fc13c8d670afdca6c
Anubhavi <anubhavi0088@gmail.com>
2026-03-23 14:42:25 +0530

    Implement log analyzer in script4.sh
    Added a log analyzer script to count error lines in syslog.

--- script4.sh
+++ script4.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
-echo "This is Script 4"
+# Script 4: Log Analyzer
+
+LOGFILE="/var/log/syslog"
+COUNT=0
+
+while read line
+do
+    if echo "$line" | grep -i "error" > /dev/null
+    then
+        COUNT=$((COUNT+1))
+    fi
+done < $LOGFILE
+
+echo "Total ERROR lines: $COUNT"
