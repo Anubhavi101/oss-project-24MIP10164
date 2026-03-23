56878ad59fe9accccb40706d6b543c1d945a455f
Anubhavi <anubhavi0088@gmail.com>
2026-03-23 14:42:02 +0530

    Enhance script3.sh with auditing functionality
    Added a disk and permission auditing script.

--- script3.sh
+++ script3.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-echo "This is Script 3"
+# Script 3: Disk and Permission Auditor
+
+DIRS=("/etc" "/home" "/var/log")
+
+for DIR in "${DIRS[@]}"
+do
+    echo "Checking $DIR"
+    ls -ld $DIR
+    du -sh $DIR
+    echo "----------------------"
+done
