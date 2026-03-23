6c7d13ec4ca74812a0e0514f0a8961fbce3e0928
Anubhavi <anubhavi0088@gmail.com>
2026-03-23 19:51:06 +0530

    Refactor FOSS Package Inspector script
    Updated package check logic and added package philosophy notes.

--- script2.sh
+++ script2.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
-# Script 2: Package Checker
+# Script 2: FOSS Package Inspector (Git-focused)
 
 PACKAGE="git"
 
-if dpkg -l | grep -q $PACKAGE; then
-    echo "$PACKAGE is installed"
-    git --version
+# Check if package is installed
+if rpm -q $PACKAGE &>/dev/null; then
+    echo "$PACKAGE is installed."
+    rpm -qi $PACKAGE | grep -E 'Version|License|Summary'
 else
-    echo "$PACKAGE is NOT installed"
+    echo "$PACKAGE is NOT installed."
 fi
+
+# Philosophy note based on package
+case $PACKAGE in
+    httpd) echo "Apache: the web server that built the open internet" ;;
+    mysql) echo "MySQL: open source at the heart of millions of apps" ;;
+    git) echo "Git: distributed version control empowering collaboration and transparency" ;;
+    vlc) echo "VLC: freedom to play any media, anywhere" ;;
+    firefox) echo "Firefox: privacy-focused open web browsing" ;;
+    *) echo "FOSS: software built by communities for everyone" ;;
+esac
