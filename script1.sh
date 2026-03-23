b547e8b008249ffdaa356c5f3dec8e36d46a04c1
Anubhavi <anubhavi0088@gmail.com>
2026-03-23 14:41:18 +0530

    Enhance script1.sh with system identity report
    Added system identity report details to the script.

--- script1.sh
+++ script1.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
-echo "This is Script 1"
+# Script 1: System Identity Report
+
+echo "==============================="
+echo " Open Source Audit"
+echo "==============================="
+
+KERNEL=$(uname -r)
+USER_NAME=$(whoami)
+UPTIME=$(uptime -p)
+DATE=$(date)
+
+echo "Kernel Version: $KERNEL"
+echo "Current User: $USER_NAME"
+echo "System Uptime: $UPTIME"
+echo "Current Date: $DATE"
