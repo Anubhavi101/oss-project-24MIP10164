395af5b2542968314e2b7cb6b9c96ddac763842a
Anubhavi <anubhavi0088@gmail.com>
2026-03-29 16:00:36 +0530

    Update package check for Ubuntu and simplify output

--- script2.sh
+++ script2.sh
@@ -1,22 +1,15 @@
 #!/bin/bash
-# Script 2: FOSS Package Inspector (Git-focused)
+# Script 2: FOSS Package Inspector
 
 PACKAGE="git"
 
-# Check if package is installed
-if rpm -q $PACKAGE &>/dev/null; then
+# Check if package is installed (Ubuntu uses dpkg)
+if dpkg -s $PACKAGE &>/dev/null; then
     echo "$PACKAGE is installed."
-    rpm -qi $PACKAGE | grep -E 'Version|License|Summary'
+    dpkg -s $PACKAGE | grep -E 'Version|Maintainer|Description'
 else
     echo "$PACKAGE is NOT installed."
 fi
 
-# Philosophy note based on package
-case $PACKAGE in
-    httpd) echo "Apache: the web server that built the open internet" ;;
-    mysql) echo "MySQL: open source at the heart of millions of apps" ;;
-    git) echo "Git: distributed version control empowering collaboration and transparency" ;;
-    vlc) echo "VLC: freedom to play any media, anywhere" ;;
-    firefox) echo "Firefox: privacy-focused open web browsing" ;;
-    *) echo "FOSS: software built by communities for everyone" ;;
-esac
+# Philosophy note
+echo "Git: distributed version control enabling collaboration."
